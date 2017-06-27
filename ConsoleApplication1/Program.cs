9a6b5b02b06d1ae26199c060ba7f15776fbed3de
narendran.venkat <narendran.venkat@PC332>
2017-06-27 14:03:35 +0530

    Add project files.

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        private string GetTextFromPDF()
+        {
+            StringBuilder text = new StringBuilder();
+            using (PdfReader reader = new PdfReader(@"C:\Users\narendran.venkat\Desktop\Avastin_en_GB.pdf"))
+            {
+                for (int i = 1; i <= reader.NumberOfPages; i++)
+                {
+                    text.Append(PdfTextExtractor.GetTextFromPage(reader, i));
+                }
+            }
+
+            return text.ToString();
+        }
+        static void Main()
+        {
+            Program pdftotext = new Program();
+            Console.WriteLine(pdftotext.GetTextFromPDF());
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
